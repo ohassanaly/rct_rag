db5b64293f3902f7e2527129ccd82cd84ce9c37a
ohassanaly <olivierhass@gmail.com>
2025-10-02 18:45:52 +0200

    adding log handlers for production ; still need to test it in deployment

--- rct_rag/logger.py
+++ rct_rag/logger.py
@@ -1,20 +1,13 @@
 import logging
 import sys
-
-# from dotenv import load_dotenv
-# import os
-
-# from logtail import LogtailHandler #better stack
-
-# load_dotenv()
-
 from pathlib import Path
 
 path = Path('.')
-log_path = path / "rct_rag/test.log"
+log_path = path / "rct_rag/test.log" #dev mode ; in deployment, logs are expected in sys.stdout
 
 #get logger
 logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 #create formater
 formatter = logging.Formatter(
@@ -23,37 +16,19 @@ formatter = logging.Formatter(
 
 #create handlers
 stream_handler = logging.StreamHandler(sys.stdout)
+stderr_handler = logging.StreamHandler(sys.stderr)
 file_handler = logging.FileHandler(log_path)
-# better_stack_handler = LogtailHandler(source_token=os.getenv("better_stack_token"), host = os.getenv("better_stack_host"))
 
 #set formatters
 stream_handler.setFormatter(formatter)
+stderr_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
 
+# Route different levels appropriately for the production log handlers
+stream_handler.addFilter(lambda record: record.levelno < logging.ERROR)
+stderr_handler.addFilter(lambda record: record.levelno >= logging.ERROR)
+
 #add handlers to the logger
-logger.handlers = [stream_handler, file_handler]
-
-# logger.handlers = [stream_handler, file_handler, better_stack_handler]
-
-#set log-level
-logger.setLevel(logging.INFO)
+logger.handlers = [stream_handler, stderr_handler, file_handler]
 
 
-#other experiments
-# from loguru import logger
-# from logtail import LogtailHandler #better stack
-
-
-# from pathlib import Path
-
-# # from dotenv import load_dotenv
-# # import os
-# # load_dotenv()
-
-# # logger.remove(0)
-# # better_stack_handler = LogtailHandler(source_token=os.getenv("better_stack_token"), host = os.getenv("better_stack_host"))
-# # logger.add(sink = better_stack_handler, serialize = True, level="INFO") #serialize = True ignores formatting
-
-# path = Path('.')
-# log_path = path / "rct_rag/test.log"
-# logger.add(sink = log_path, serialize = True, level="TRACE")
